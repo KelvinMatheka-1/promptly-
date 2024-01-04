c74110a189aed4288b7ce84ce1d4d17c0e411396
Kelvin M Matheka <beingmatheka@gmail.com>
2024-01-05 00:49:34 +0300

    api

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -2,11 +2,12 @@ import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
 const handler = NextAuth({
-  PROVIDERS: [
+  providers: [
 
     GoogleProvider({
-      clientId: '',
-      clientSecret: ''
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  
     })
   ],
   async session ({ session }){
