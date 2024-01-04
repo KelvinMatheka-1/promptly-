34322d735e66b765f69b5f0274b019ee8bd2fc8d
Kelvin M Matheka <beingmatheka@gmail.com>
2024-01-04 15:08:55 +0300

    navbar

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,4 @@
+import Feed from '@components/Feed'
 
 const Home = () => {
   return (
@@ -12,7 +13,7 @@ const Home = () => {
       <p className="desc text-center">
         Promptopia is an open source AI prompting tool for modern world to discover, create and share creative prompts
       </p>
-      {/* {feed} */}
+      <Feed />
     </section>
   )
 }
