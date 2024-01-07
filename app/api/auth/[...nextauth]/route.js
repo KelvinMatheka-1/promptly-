ac1d4ff50019cdf493f8f0c266fa753ba719e672
Kelvin M Matheka <beingmatheka@gmail.com>
2024-01-07 23:38:54 +0300

    finding user by email

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,7 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 import { connectToDB } from '@utils/database';
+import User from '@models/user';
  
 const handler = NextAuth({
   providers: [
@@ -17,6 +18,9 @@ const handler = NextAuth({
   async signIn({ profile }){
     try{
       await connectToDB();
+
+      //check if a user exists
+      const userExists =  await User.findOne({ email: profile.email });
 
       return true;
     }catch(error){
