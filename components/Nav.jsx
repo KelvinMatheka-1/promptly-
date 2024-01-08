527d57d6b3873ee56415fcf3df4eff08fd0b5c47
Kelvin M Matheka <beingmatheka@gmail.com>
2024-01-08 17:32:44 +0300

    google picture

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -41,7 +41,7 @@ const Nav = () => {
               Sign Out
             </button>
             <Link href="/profile">
-             <Image src="/assets/images/logo.svg" width={37} height={37} className='rounded-full' alt='profile' />
+             <Image src={session?.user.image} width={37} height={37} className='rounded-full' alt='profile' />
             </Link>
           </div>
         ): (
