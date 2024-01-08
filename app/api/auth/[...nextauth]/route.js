6911199ccd7a7176aedc4278f640457bbf278246
Kelvin M Matheka <beingmatheka@gmail.com>
2024-01-08 20:45:36 +0300

    fix route

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -12,36 +12,39 @@ const handler = NextAuth({
   
     })
   ],
-  async session ({ session }){
-    const sessionUser = await User.findOne({
-      email: session.user.email
-    })
 
-    session.user.id = session.User._id.toString();
-
-    return session;
-
-  },
-  async signIn({ profile }){
-    try{
-      await connectToDB();
-
-      //check if a user exists
-      const userExists =  await User.findOne({ email: profile.email });
-      //if not, create new user
-      if (!userExists){
-        await User.create({
-          email: profile.email,
-          username: profile.name.replace(" ", "").toLowercase(),
-          image: profile.picture
-        })
+  callbacks: {
+    async session ({ session }){
+      const sessionUser = await User.findOne({
+        email: session.user.email
+      })
+  
+      session.user.id = session.User._id.toString();
+  
+      return session;
+  
+    },
+    async signIn({ profile }){
+      try{
+        await connectToDB();
+  
+        //check if a user exists
+        const userExists =  await User.findOne({ email: profile.email });
+        //if not, create new user
+        if (!userExists){
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowercase(),
+            image: profile.picture
+          })
+        }
+  
+        return true;
+  
+      }catch(error){
+        console.log(error);
+        return false;
       }
-
-      return true;
-
-    }catch(error){
-      console.log(error);
-      return false;
     }
   }
 })
