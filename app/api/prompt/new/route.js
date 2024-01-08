2a14eb5cd9b4aca996de1a02ca37f00833650d1b
Kelvin M Matheka <beingmatheka@gmail.com>
2024-01-09 00:30:29 +0300

    api route

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -8,13 +8,14 @@ export const POST = async (req) => {
     await connectToDB();
     const newPrompt = new Prompt ({ 
       creator: userId, 
+      prompt,
       tag
     })
 
     await newPrompt.save();
 
-    return new Response(JSON.stringify(newPrompt), {status: 201})
+    return new Response(JSON.stringify(newPrompt), { status: 201 })
   }catch (errorr) {
-
+    return new Response('Failed to create a new prompt', { status: 500})
   }
 }
