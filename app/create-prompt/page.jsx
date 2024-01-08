aa80915738b554e8aa671109ddedd5116f382e4d
Kelvin M Matheka <beingmatheka@gmail.com>
2024-01-08 23:46:00 +0300

    prompt

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -4,6 +4,7 @@ import { useState } from 'react'
 import { useSession } from 'next-auth/react'
 import { useRouter } from 'next/navigation'
 import Form from '@components/Form'
+import { POST } from '@app/api/auth/[...nextauth]/route'
 
 const CreatePrompt = () => {
   const [submitting, setsubmitting] = useState(false);
@@ -14,7 +15,28 @@ const CreatePrompt = () => {
   });
 
   const createPrompt = async (e) => {
+    e.preventDefault();
 
+    setsubmitting(true);
+
+    try{
+      const response = await fetch('/api/prompt/new', {
+        method: POST,
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag
+        })
+      })
+      if(response.ok){
+        RouterContext.push('/');
+      }
+
+    }catch(error){
+      console.log(error);
+    }finally{
+      setsubmitting(false);
+    }
   }
 
   return (
