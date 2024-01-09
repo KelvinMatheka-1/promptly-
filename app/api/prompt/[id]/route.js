0d93636023b5ddccc5271a877272065e148d919e
Kelvin M Matheka <beingmatheka@gmail.com>
2024-01-09 23:06:02 +0300

    route.js UPDATE

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -1,7 +1,7 @@
 import { connectToDB } from "@utils/database";
 import Prompt from "@models/prompt";
 
-
+//get
 export const GET = async (request, { params }) => {
   try {
     await connectToDB();
@@ -20,3 +20,25 @@ export const GET = async (request, { params }) => {
   }
 };
 
+//update
+
+export const PATCH = async (request, { params }) => {
+  const { prompt, tag } =  await request.json();
+
+  try{
+    await connectToDB();
+    const existingPrompt = await Prompt.findById(params.id);
+
+    if(!existingPrompt) return new Response("prompt not found", { status: 404 });
+
+    existingPrompt.prompt = prompt;
+    existingPrompt.tag = tag;
+
+    await existingPrompt.save();
+
+    return new Response(JSON.stringify(existingPrompt), { status: 200 });
+
+  } catch(error){
+    return new Response("failed to update prompt", { status: 500 });
+  }
+}
